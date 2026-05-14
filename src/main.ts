5682b5e6a693cd90242c599f6775d094e6a93195
sara91-dev <sarabog91@gmail.com>
2026-05-14 11:28:19 +0200

    pequeños cambios + hamburguer menú

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,22 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <section id="header" class="bg-slate-800/70 backdrop-blur-lg sticky top-0 z-50">
   <div class="">
     <ul class="text-white flex justify-center gap-24 lg:gap-36 font-semibold py-6 sm:py-4 tracking-wide text-sm">
-        <li class="sm:hidden absolute left-5 top-2.5 item-start cursor-pointer bg-white h-8 w-8 rounded-lg"></li>
+        
+        <button id="menuBtn" class="sm:hidden absolute left-6 top-2 h-8 w-8 cursor-pointer bg-white/90 backdrop-blur-md shadow-md rounded-xl flex items-center justify-center transition-all duration-200 hover:bg-white hover:shadow-lg hover:scale-105 active:scale-95 focus:outline-none">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M4 6h16M4 12h16M4 18h16"/>
+          </svg>
+        </button>
+        
+        <nav id="mobileMenu" class="absolute z-50 text-black top-14 left-6 bg-white shadow-lg rounded-xl p-4 opacity-0 scale-95 pointer-events-none transition-all duration-200">          
+          <ul class="space-y-2">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
+        </nav>
+
+        
         <li class="hidden sm:block absolute left-5 top-1.5 item-start cursor-pointer h-10 w-10 rounded-lg">
             <img src="./logo.png" alt="logo" class="h-full w-full object-cover rounded-lg"/>
         </li>
@@ -17,8 +32,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </ul>
   </div>
 </section>
+<div id="overlay" class="fixed inset-0 bg-black/30 backdrop-blur-sm opacity-0 pointer-events-none transition-opacity duration-200 z-40"></div>
 
 <section id="body" class="mt-20 md:mt-34">
+
     <div class="flex items-center flex-col md:flex-row justify-center gap-10 md:gap-30 lg:gap-60 xl:gap-100 2xl:gap-120 mx-6">
         <div class="flex flex-col justify-center">
             <h1 class="flex justify-center text-xl md:text-2xl font-semibold tracking-wider bg-gradient-to-r from-slate-700 to-slate-400 bg-clip-text text-transparent ">
@@ -35,10 +52,16 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
             </span>
             </div>
                                 
-        <div class="text-center bg-black w-60 h-30 md:w-120 md:h-72 rounded-lg">
+        <div class="relative overflow-hidden w-2/3 aspect-video md:w-1/2 lg:w-2/5 xl:w-1/3 rounded-lg bg-black">
+
+            <img src="img1.jpg" class="slide absolute w-full h-full object-cover transition-opacity duration-700 opacity-100">
+            <img src="img2.jpg" class="slide absolute w-full h-full object-cover transition-opacity duration-700 opacity-0">
+            <img src="img3.jpg" class="slide absolute w-full h-full object-cover transition-opacity duration-700 opacity-0">
+            
         </div>
         
     </div>
+    
     
     <div class="flex items-baseline justify-center gap-1.5 mt-12 md:mt-20 tracking-wide">
         <p class="text-gray-800 text-xs md:text-sm">Quieres</p>
@@ -81,38 +104,38 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     
     <div class="flex flex-col items-center mt-20 md:mt-28 mb-20">
         <h3 class="text-gray-500 text-xl font-semibold tracking-wider mb-12">Te interesa?</h3>
-            <div class="border border-white bg-slate-100 shadow-inner w-100 h-72 md:w-120 lg:w-140 xl:h-76 xl:w-160 2xl:w-180 rounded-lg">
+            <div class="border border-white bg-slate-100 shadow-inner w-100 h-76 md:w-120 lg:w-140 xl:h-76 xl:w-160 2xl:w-180 rounded-lg">
                 <ul class="m-4 text-sm tracking-wide">
                     <li class="relative flex items-center justify-center mt-8 mb-4 mx-4 bg-slate-600 border border-slate-200 p-1.5 rounded-lg text-white font-semibold text-center">
                         <svg class="absolute left-3 item-start w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
                           <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
-                        <span>fdfsdfd</span>
+                        <span>Personalizable</span>
                     </li>
                     
                     <li class="relative flex items-center justify-center mb-4 mx-4 bg-white border border-slate-200 p-1.5 rounded-lg text-slate-600 font-semibold text-center">
                     <svg class="absolute left-3 item-start w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m0-10.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.25-8.25-3.286Zm0 13.036h.008v.008H12v-.008Z" />
                     </svg>
-                        <span>fdfsdfd</span>
+                        <span>Seguro</span>
                     </li>
                     
                     <li class="relative flex items-center justify-center mb-4 mx-4 bg-slate-600 border border-slate-200 p-1.5 rounded-lg text-white font-semibold text-center">
                         <svg class="absolute left-3 item-start w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                         </svg>
-                        <span>fdfsdfd</span>
+                        <span>Soporte directo por Whatsapp</span>
                     </li>
                     
                     <li class="relative flex items-center justify-center mb-4 mx-4 bg-white border border-slate-200 p-1.5 rounded-lg text-slate-600 font-semibold text-center">
                         <svg class="absolute left-3 item-start w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
                         </svg>
-                        <span>fdfsdfd</span>
+                        <span>Rápido</span>
                     </li>
                 </ul>
-                <button class="block mx-auto cursor-pointer mt-4 xl:mt-6 px-6 py-2 bg-gray-200 border border-gray-300 text-gray-700 font-semibold rounded-full text-sm tracking-wider transition-all duration-200 hover:bg-gray-300 hover:shadow-md active:scale-95">
+                <button class="block mx-auto cursor-pointer mt-6 xl:mt-6 px-6 py-2 bg-gray-200 border border-gray-300 text-gray-700 font-semibold rounded-full text-sm tracking-wider transition-all duration-200 hover:bg-gray-300 hover:shadow-md active:scale-95">
                   !Háblanos!
                 </button>
             </div>
@@ -254,7 +277,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
             <p class="text-white text-sm ">© 2026 GRebelAir® Todos los derechos reservados.</p>
             <p class="text-white text-sm ">GRebelAir es una marca registrada.</p>
         </div>
-        <div class= "flex flex-col items-center lg:flex-row mr-8 lg:mr-4 gap-1.5 text-white text-sm font-semibold">
+        <div class= "flex flex-col items-center lg:flex-row mr-4 gap-1.5 text-white text-sm font-semibold">
             <a class="lg:mr-12 hover:text-blue-300 hover:underline hover:decoration-1" href="/">Privacidad</a>
             <a class="lg:mr-12 hover:text-blue-300 hover:underline hover:decoration-1" href="/">Cookies</a>
             <a class="lg:mr-2 hover:text-blue-300 hover:underline hover:decoration-1" href="/">Aviso legal</a>
@@ -264,7 +287,45 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 
 `
+const btn = document.getElementById("menuBtn") as HTMLButtonElement;
+const menu = document.getElementById("mobileMenu") as HTMLDivElement;
+const overlay = document.getElementById("overlay") as HTMLDivElement;
 
+let open = false;
+
+btn.addEventListener("click", () => {
+  open = !open;
+
+  document.body.classList.toggle("overflow-hidden", open);
+
+  menu.classList.toggle("opacity-0", !open);
+  menu.classList.toggle("scale-95", !open);
+  menu.classList.toggle("pointer-events-none", !open);
+
+  overlay.classList.toggle("opacity-0", !open);
+  overlay.classList.toggle("pointer-events-none", !open);
+});
+
+overlay.addEventListener("click", () => {
+  open = false;
+
+  document.body.classList.remove("overflow-hidden");
+
+  menu.classList.add("opacity-0", "scale-95", "pointer-events-none");
+  overlay.classList.add("opacity-0", "pointer-events-none");
+});
+
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+setInterval(() => {
+    slides[current].classList.add("opacity-0");
+    current = (current + 1) % slides.length;
+    slides[current].classList.remove("opacity-0");
+}, 3000);
+   
+   
+    
 const button = document.getElementById("emailInfoBtn") as HTMLButtonElement;
 const emailInput = document.getElementById("emailInput") as HTMLInputElement;
 const errorMsg = document.getElementById("errorMsg");
