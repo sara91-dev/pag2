f29803c4d54e0e4c98e7f1914b84b88b519b6cdc
sara91-dev <sarabog91@gmail.com>
2026-05-19 17:38:59 +0200

    formulario añadido

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -6,7 +6,7 @@ export function renderFooter(): string {
             <p class="text-white text-sm ">© 2026 GRebelAir® Todos los derechos reservados.</p>
             <p class="text-white text-sm ">GRebelAir es una marca registrada.</p>
         </div>
-        <div class= "flex flex-col items-center lg:flex-row mr-4 gap-1.5 text-white text-sm font-semibold">
+        <div class= "flex flex-col sm:items-center lg:flex-row mr-4 gap-1.5 text-white text-sm font-semibold">
             <a class="lg:mr-12 hover:text-blue-300 hover:underline hover:decoration-1" href="/pag2/privacidad" data-link>Política de privacidad</a>
             <a class="lg:mr-12 hover:text-blue-300 hover:underline hover:decoration-1" href="/pag2/cookies" data-link>Política de cookies</a>
             <a class="lg:mr-2 hover:text-blue-300 hover:underline hover:decoration-1" href="/pag2/aviso-legal" data-link>Aviso legal</a>
--- src/pages/formulario.ts
+++ src/pages/formulario.ts
@@ -1,6 +1,7 @@
 import { renderHeader } from "../components/header";
 import { renderFooter } from "../components/footer";
 
+
 export function renderFormulario() {
   const app = document.querySelector<HTMLDivElement>('#app');
 
@@ -12,12 +13,15 @@ export function renderFormulario() {
 
     <main class="flex-1 flex items-center justify-center px-4 py-10">
       
-      <section class="w-full max-w-3xl">
-        <div class="bg-white rounded-2xl shadow-xl p-8 md:p-10 md:pt-8 border border-gray-200">
+      <section class="w-full max-w-lg md:max-w-xl lg:max-w-2xl p-4">
+        <div class="bg-white rounded-3xl shadow-xl p-8 md:p-10 md:pt-8 border border-gray-200">
           
-          <h1 class="text-center text-2xl font-bold mb-10 text-gray-800">
+          <h1 class="text-center tracking-wider text-2xl font-bold pb-4 text-gray-800">
             Formulario
           </h1>
+          <div class="flex justify-center mb-10">
+              <div class="w-20 h-1 bg-gray-400 rounded-full"></div>
+          </div>
 
           <form class="space-y-6">
 
@@ -27,7 +31,7 @@ export function renderFormulario() {
               </label>
               <input 
                 type="text"
-                class="w-full border border-gray-300 rounded-full px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                class="text-sm text-center w-full border border-gray-300 rounded-full px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Escribe tu nombre"
               />
             </div>
@@ -38,7 +42,7 @@ export function renderFormulario() {
               </label>
               <input 
                 type="email"
-                class="w-full border border-gray-300 rounded-full px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                class="w-full text-sm text-center border border-gray-300 rounded-full px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="correo@ejemplo.com"
               />
             </div>
@@ -49,18 +53,18 @@ export function renderFormulario() {
               </label>
               <textarea
                 rows="5"
-                class=" w-full border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                class="text-center text-sm w-full border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Escribe tu mensaje..."
               ></textarea>
             </div>
-
-            <button
-              type="submit"
-              class="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition"
-            >
-              Enviar
-            </button>
-
+            <div class="flex justify-center">
+                <button
+                  type="submit"
+                  class="tracking-wide px-6 bg-gray-600/50 border border-gray-300 shadow-md hover:bg-blue-700 text-white font-semibold py-1 rounded-full transition"
+                >
+                  Enviar
+                </button>
+            </div>
           </form>
 
         </div>
